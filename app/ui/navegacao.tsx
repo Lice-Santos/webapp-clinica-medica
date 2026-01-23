830d42345895ea39f7ea29bb9f34c2e29a375c29
Lice-Santos <alicenuunes05@gmail.com>
2026-01-23 19:47:13 -0300

    feat: sign out adicionado

--- app/ui/navegacao.tsx
+++ app/ui/navegacao.tsx
@@ -1,6 +1,30 @@
+"use client";
+
 import Link from "next/link";
+import { handleSignOut } from "../utils/signOut";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
 
 export default function BarraDeNavegacao() {
+  const router = useRouter();
+
+  const abrirModalConfirmacao = () => {
+   Swal.fire({
+      title: "Deseja sair da aplicação?",
+      showDenyButton: true,
+      showCancelButton: false,
+      denyButtonText: "Sair",
+      confirmButtonText: "Continuar na aplicação",
+    }).then((result) => {
+      if (result.isDenied) {
+
+        handleSignOut();
+        router.push('/')
+      } 
+      
+    });
+  };
+  
   return (
     <aside className="w-55 min-h-screen bg-primary-500 flex flex-col p-4 gap-6">
       <h2 className="text-white text-xl font-bold">Menu</h2>
@@ -26,6 +50,9 @@ export default function BarraDeNavegacao() {
           Histórico de Consultas
         </Link>
 
+        <button onClick={() => abrirModalConfirmacao()} className="text-white hover:opacity-80 text-left cursor-pointer">
+          Sair
+        </button>
       </nav>
     </aside>
   );
